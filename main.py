aa3955d9a83ff72f5412d0a748391502efb7548a
feelit-fill <209713307+feelit-fill@users.noreply.github.com>
2025-05-05 08:05:51 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import json
+
+#棒金の枚数
+ROLL_COIN_COUNT = 50
+
+#金庫読み込み
+with open("safe.json", "r", encoding="utf8") as f:
+    safe = json.load(f)
+
+#string型データをint型に変換する関数
+def parse_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return 0
+
+
+#クラスの定義-----
+class Money:
+    def __init__(self,amount,count):
+        self.amount = amount #お金の価値
+        self.count = count #枚数
+
+class RollCoin:
+    def __init__(self,amount,count):
+        self.amount = amount #お金の価値
+        self.count = count #本数
+    
+    #棒金をバラ硬貨へ換算して返すメソッド
+    def convert_rolls_to_coins(self):
+        return int(self.count * ROLL_COIN_COUNT)
+
+
+#要素の配置----------------
+st.title("レジ金登録")
+#3カラム構成の設定
+col_bill,col_roll,col_coin = st.columns(3)
+
+#入力欄の配置
+with col_bill:
+    input_man = parse_int(st.text_input("10000円の枚数"))
+    input_gosen = parse_int(st.text_input("5000円の枚数"))
+    input_sen = parse_int(st.text_input("1000円の枚数"))
+
+with col_roll:
+    input_r_five_hundreds = parse_int(st.text_input("【棒金】500円の本数"))
+    input_r_hundreds = parse_int(st.text_input("【棒金】100円の本数"))
+    input_r_fifty = parse_int(st.text_input("【棒金】50円の本数"))
+    input_r_ten = parse_int(st.text_input("【棒金】10円の本数"))
+    input_r_five = parse_int(st.text_input("【棒金】5円の本数"))
+    input_r_one = parse_int(st.text_input("【棒金】1円の本数"))
+
+with col_coin:
+    input_five_hundreds = parse_int(st.text_input("500円の枚数"))
+    input_hundreds = parse_int(st.text_input("100円の枚数"))
+    input_fifty = parse_int(st.text_input("50円の枚数"))
+    input_ten = parse_int(st.text_input("10円の枚数"))
+    input_five = parse_int(st.text_input("5円の枚数"))
+    input_one = parse_int(st.text_input("1円の枚数"))
+
+
+#取得した枚数を使ってインスタンスを生成
+man_yen = Money(10000,input_man)
+gosen_yen = Money(5000,input_gosen)
+sen_yen = Money(1000,input_sen)
+
+r_five_hundreds_yen = RollCoin(500,input_r_five_hundreds)
+r_hundreds_yen = RollCoin(100,input_r_hundreds)
+r_fifty_yen = RollCoin(50,input_r_fifty)
+r_ten_yen = RollCoin(10,input_r_ten)
+r_five_yen = RollCoin(5,input_r_five)
+r_one_yen = RollCoin(1,input_r_one)
+
+five_hundreds_yen = Money(500,input_five_hundreds)
+hundred_yen = Money(100,input_hundreds)
+fifty_yen = Money(50,input_fifty)
+ten_yen = Money(10,input_ten)
+five_yen = Money(5,input_five)
+one_yen = Money(1,input_one)
+
+
+#1レジのお金の枚数辞書作成
+first_register = {
+    "10000円": man_yen.count,
+    "5000円": gosen_yen.count,
+    "1000円": sen_yen.count,
+    "500円": five_hundreds_yen.count + r_five_hundreds_yen.convert_rolls_to_coins(),
+    "100円": hundred_yen.count + r_hundreds_yen.convert_rolls_to_coins(),
+    "50円": fifty_yen.count + r_fifty_yen.convert_rolls_to_coins(),
+    "10円": ten_yen.count + r_ten_yen.convert_rolls_to_coins(),
+    "5円": five_yen.count + r_five_yen.convert_rolls_to_coins(),
+    "1円": one_yen.count + r_one_yen.convert_rolls_to_coins(),
+}
+
+#safe と first_registerを足し合わせる処理
+result = {}
+for key in safe:
+    result[key] = safe[key] + first_register[key]
+
+st.write(result)
+st.session_state["result"] = result
---
+++ run_money_counter.command
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /Users/manatone/Documents/programing/python/Money_counter
+source .venv/bin/activate
+streamlit run main.py
